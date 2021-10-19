0b740379188924e08f4d894bdb826785bb43b472
mauroesc <mauroesc@users.noreply.github.com>
2021-10-19 17:26:07 -0300

    upload inicial

---
+++ dist/precache-manifest.aa3c8037a37d06d17e09656e93f13658.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddefcfa5a5eda695fbf",
+    "url": "/css/chunk-17bc2355.ea0b1567.css"
+  },
+  {
+    "revision": "e35530797e8832da83d3",
+    "url": "/css/chunk-vendors.4abf34fe.css"
+  },
+  {
+    "revision": "60dacc1b6209660cde6df24619ff81e7",
+    "url": "/img/bg.60dacc1b.jpg"
+  },
+  {
+    "revision": "7c6b545e43099d35e2402d2ad27094b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b8cc2eabcc141524ea",
+    "url": "/js/app.985122fa.js"
+  },
+  {
+    "revision": "3ddefcfa5a5eda695fbf",
+    "url": "/js/chunk-17bc2355.9ff9aa0a.js"
+  },
+  {
+    "revision": "076d34956eb3df146f62",
+    "url": "/js/chunk-2d22d746.be27f1a3.js"
+  },
+  {
+    "revision": "e35530797e8832da83d3",
+    "url": "/js/chunk-vendors.45453823.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tarefas: [
+      {id: 1, titulo: "Ir ao mercado", concluido: false },
+      {id: 2, titulo: "Comprar ração", concluido: false },
+    ]
+
+  },
+  mutations: {
+
+    adicionaTarefa(state, titulo){
+      if (titulo){
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          concluido: false
+        });
+        this.campoInput = null;
+      }
+    },
+
+    removeTarefa(state, id){
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id);
+    }
+
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
